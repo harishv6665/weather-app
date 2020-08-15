6f34c27fac68829f2fe24aa72560b062e8890846
Harish <h.venkataramanappa@talentry.com>
2020-08-15 13:53:30 +0200

    build push

---
+++ build/precache-manifest.8719d2197240099d49ae270ba84aed33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1d62c4e48a158823a0b91fe78cd732",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "09e5b43d37eac65393cf",
+    "url": "/weather-app/static/css/main.25d697e4.chunk.css"
+  },
+  {
+    "revision": "6d2a74df50c6141ef1d6",
+    "url": "/weather-app/static/js/2.91f68cde.chunk.js"
+  },
+  {
+    "revision": "82e3c575391da7a71a1694351f425a33",
+    "url": "/weather-app/static/js/2.91f68cde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09e5b43d37eac65393cf",
+    "url": "/weather-app/static/js/main.8a50dc3a.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
